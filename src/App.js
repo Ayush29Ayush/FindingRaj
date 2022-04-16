dfc8a089eb5cb786b2fa5271e030d8d6ef5cbc50
Ayush Senapati <ayushsenapati123@gmail.com>
2022-04-16 19:08:52 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 
@@ -8,7 +11,14 @@ function App() {
       <div className="flex flex-col justify-between h-screen">
         <Navbar title="Finding Raj" />
 
-        <main className="container mx-auto px-3 pb-12">Content</main>
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
 
         <Footer />
       </div>
