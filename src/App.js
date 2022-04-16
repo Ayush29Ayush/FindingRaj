743d1a822233b028a5f2b7ea474f86e861aab7e8
Ayush Senapati <ayushsenapati123@gmail.com>
2022-04-16 18:40:14 +0530

    added Footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
-        <Navbar title="Finding Raj"/>
+        <Navbar title="Finding Raj" />
 
         <main>Content</main>
+
+        <Footer />
       </div>
     </Router>
   );
