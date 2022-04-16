dcabfbd4eaa6bf417f7e978ed25444ca6df42c08
Ayush Senapati <ayushsenapati123@gmail.com>
2022-04-16 19:16:17 +0530

    about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,19 @@
 function About() {
   return (
-    <div>About</div>
-  )
+    <div>
+      <h1 className="text-6xl mb-4">Finding Raj</h1>
+      <p className="mb-4 text-2xl font-light">
+        A React app to search GitHub profiles and see profile details.
+      </p>
+      <p className="text-lg text-gray-400">
+        Version <span className="text-white">1.0.0</span>
+      </p>
+      <p className="text-lg text-gray-400">
+        Created By:
+        <span className="text-white"> Ayush Senapati</span>
+      </p>
+    </div>
+  );
 }
 
-export default About
+export default About;
