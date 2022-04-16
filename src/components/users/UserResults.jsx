5d5d7f73a45367a861263757d7fe15274d3b6b1a
Ayush Senapati <ayushsenapati123@gmail.com>
2022-04-16 23:21:36 +0530

    UserResults

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -18,10 +18,20 @@ function UserResults() {
     const data = await response.json();
 
     setUsers(data);
-    setLoading(false)
+    setLoading(false);
   };
 
-  return <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg: grid-cols-3 md: grid-cols-2">UserResults</div>;
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg: grid-cols-3 md: grid-cols-2">
+        {users.map((user) => (
+          <h3>{user.login}</h3>
+        ))}
+      </div>
+    );
+  } else {
+      return <h3>Loading...</h3>
+  }
 }
 
 export default UserResults;
