660aeb4045fb1ef0d8157d7e68f0ac657358b9d1
Ayush Senapati <ayushsenapati123@gmail.com>
2022-04-17 15:50:51 +0530

    testing

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -14,6 +14,7 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
+  //! Get initial users (testing purposes)
   const fetchUsers = async () => {
     setLoading();
 
