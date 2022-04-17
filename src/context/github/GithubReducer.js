9acf323fbf8e72a14184c169b247dde53ced6703
Ayush Senapati <ayushsenapati123@gmail.com>
2022-04-17 15:11:44 +0530

    reducer for GET_USERS

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -1,5 +1,11 @@
 const githubReducer = (state, action) => {
   switch (action.type) {
+    case "GET_USERS":
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+      };
     default:
       return state;
   }
