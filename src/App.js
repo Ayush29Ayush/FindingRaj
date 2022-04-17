bb1923a08a684d4e165fe79806292a45199ec3af
Ayush Senapati <ayushsenapati123@gmail.com>
2022-04-17 14:09:03 +0530

    formatting

--- src/App.js
+++ src/App.js
@@ -4,27 +4,27 @@ import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
-import {GithubProvider} from './context/github/GithubContext'
+import { GithubProvider } from "./context/github/GithubContext";
 
 function App() {
   return (
     <GithubProvider>
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar title="Finding Raj" />
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar title="Finding Raj" />
 
-        <main className="container mx-auto px-3 pb-12">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/notfound" element={<NotFound />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/notfound" element={<NotFound />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
 
-        <Footer />
-      </div>
-    </Router>
+          <Footer />
+        </div>
+      </Router>
     </GithubProvider>
   );
 }
