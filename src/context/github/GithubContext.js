4e064a33d9fb3c6ed5d3a1f2128876a1f7d90f2b
Ayush Senapati <ayushsenapati123@gmail.com>
2022-04-18 13:40:36 +0530

    clearUsers action

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -37,6 +37,9 @@ export const GithubProvider = ({ children }) => {
     });
   };
 
+  //! Clear users from state
+  const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
+
   //! Set Loading
   const setLoading = () => dispatch({ type: "SET_LOADING" });
 
@@ -46,6 +49,7 @@ export const GithubProvider = ({ children }) => {
         users: state.users,
         loading: state.loading,
         searchUsers,
+        clearUsers
       }}
     >
       {children}
