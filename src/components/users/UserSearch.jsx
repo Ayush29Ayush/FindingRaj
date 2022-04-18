ab7e9aab478055e61963cdbddeb85119bc3e2007
Ayush Senapati <ayushsenapati123@gmail.com>
2022-04-18 08:38:24 +0530

    handleSubmit for form

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,18 +1,38 @@
+import { useState, useContext } from "react";
+
 function UserSearch() {
+  const [text, setText] = useState("");
+
+  const handleChange = (e) => setText(e.target.value);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (text === '') {
+        alert('Please enter something', 'error')
+      } else {
+        //TODO - search users
+        
+        setText("")
+      }
+  };
+
   return (
     <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-control">
             <div className="relative">
               <input
                 type="text"
                 className="w-full pr-40 bg-gray-200 input input-lg text-black"
                 placeholder="Search"
+                value={text}
+                onChange={handleChange}
               />
               <button
-                type='submit'
-                className='absolute top-0 right-0 rounded-l-none w-36 btn btn-lg'
+                type="submit"
+                className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg"
               >
                 Go
               </button>
