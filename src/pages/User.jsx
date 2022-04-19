fc4adfa45c66062b768287e5d99c1c1ab98b7f57
Ayush Senapati <ayushsenapati123@gmail.com>
2022-04-20 00:00:04 +0530

    avatar and name

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -43,6 +43,21 @@ function User() {
             Back To Search
           </Link>
         </div>
+
+        <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8">
+          <div className="custom-card-image mb-6 md:mb-0">
+            <div className="rounded-lg shadow-xl card image-full">
+              <figure>
+                <img src={avatar_url} alt="" />
+              </figure>
+              <div className="card-body justify-end">
+                <h2 className="card-title mb-0">{name}</h2>
+                <p>{login}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        
       </div>
     </>
   );
