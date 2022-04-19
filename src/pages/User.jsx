6a41b3e86049e7f4ad5da334903a48d3cecdb779
Ayush Senapati <ayushsenapati123@gmail.com>
2022-04-19 23:56:51 +0530

    Back button

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useContext } from "react";
 import GithubContext from "../context/github/GithubContext";
 import { useParams } from "react-router-dom";
+import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import Spinner from "../components/layout/Spinner";
 
 function User() {
-  const { getUser, user } = useContext(GithubContext);
+  const { getUser, user, loading } = useContext(GithubContext);
 
   const params = useParams();
 
@@ -11,6 +14,37 @@ function User() {
     getUser(params.login);
   }, []);
 
-  return <div>{user.login}</div>;
+  const {
+    name,
+    type,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    twitter_username,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user;
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <>
+      <div className="w-full mx-auto lg:w-10/12">
+        <div className="mb-4">
+          <Link to="/" className="btn btn-ghost">
+            Back To Search
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
 export default User;
