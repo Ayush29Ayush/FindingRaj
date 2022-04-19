ff653fd874fe953a26b9751a8abf5809743e855b
Ayush Senapati <ayushsenapati123@gmail.com>
2022-04-19 23:37:40 +0530

    User page

---
+++ src/pages/User.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useContext } from "react";
+import GithubContext from "../context/github/GithubContext";
+import { useParams } from "react-router-dom";
+
+function User() {
+  const { getUser, user } = useContext(GithubContext);
+
+  const params = useParams();
+
+  useEffect(() => {
+    getUser(params.login);
+  }, []);
+
+  return <div>{user.login}</div>;
+}
+export default User;
