7ddb4479b11b59f93ddd1a025db88aa7a11d1675
Ayush Senapati <ayushsenapati123@gmail.com>
2022-04-20 12:19:30 +0530

    RepoList UI

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -1,7 +1,22 @@
-function RepoList() {
+import PropTypes from "prop-types";
+
+function RepoList({ repos }) {
   return (
-    <div>RepoList</div>
-  )
+    <div className="rounded-lg shadow-lg card bg-base-100">
+      <div className="card-body">
+        <h2 className="text-3xl my-4 font-bold card-title">
+          Latest Respositories
+        </h2>
+        {repos.map((repo) => (
+          <h3>{repo.name}</h3>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default RepoList
+RepoList.propTypes = {
+  repos: PropTypes.array.isRequired,
+};
+
+export default RepoList;
