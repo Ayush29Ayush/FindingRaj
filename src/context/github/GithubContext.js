ab93f4c15bb547adcbd7f25f31172bf1bc6933d2
Ayush Senapati <ayushsenapati123@gmail.com>
2022-04-22 15:20:10 +0530

    cleaning code

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -96,10 +96,11 @@ export const GithubProvider = ({ children }) => {
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        loading: state.loading,
-        user: state.user,
-        repos: state.repos,
+        // users: state.users,
+        // loading: state.loading,
+        // user: state.user,
+        // repos: state.repos,
+        ...state,
         searchUsers,
         clearUsers,
         getUser,
