3283d9a27b1a8fc9b0450c1d14c7cee904cc49e8
Ayush Senapati <ayushsenapati123@gmail.com>
2022-04-22 18:12:14 +0530

    cleaning code

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,21 +1,26 @@
-import { useEffect, useContext } from "react";
-import GithubContext from "../context/github/GithubContext";
-import { useParams } from "react-router-dom";
 import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa";
+import { useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Spinner from "../components/layout/Spinner";
 import RepoList from "../components/repos/RepoList";
+import GithubContext from "../context/github/GithubContext";
+import { getUserAndRepos } from "../context/github/GithubActions";
 
 function User() {
-  const { getUser, user, loading, getUserRepos, repos } =
-    useContext(GithubContext);
+  const { user, loading, repos, dispatch } = useContext(GithubContext);
 
   const params = useParams();
 
   useEffect(() => {
-    getUser(params.login);
-    getUserRepos(params.login);
-  }, []);
+    dispatch({ type: "SET_LOADING" });
+    const getUserData = async () => {
+      const userData = await getUserAndRepos(params.login);
+      dispatch({ type: "GET_USER_AND_REPOS", payload: userData });
+    };
+
+    getUserData();
+  }, [dispatch, params.login]);
 
   const {
     name,
@@ -168,4 +173,5 @@ function User() {
     </>
   );
 }
+
 export default User;
